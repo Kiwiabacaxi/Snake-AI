a8ba5d5e0cf8516fad477bbe9eb37857dbf68349
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-10-19 20:34:50 -0300

    feat: adicionado um modelo pronto no folder FAST

---
+++ snake/FAST/helper.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from IPython import display
+
+plt.ion()
+
+def plot(scores, mean_scores):
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+    # fazer um modelo sem GUI PRA TREINAR MAIS RAPIDO PLMDDS
+    plt.title('Treinando(tada.ED_v2)')
+    plt.xlabel('Numero de geracoes')
+    plt.ylabel('Score')
+    plt.plot(scores)
+    plt.plot(mean_scores)
+    plt.ylim(ymin=0)
+    plt.text(len(scores)-1, scores[-1], str(scores[-1]))
+    plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.show(block=False)
+    plt.pause(.1)
